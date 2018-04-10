6529fb3c3fad48d0493e278fc3b5f532ac3da811
BrentJarr <38262450+BrentJarr@users.noreply.github.com>
2018-04-10 14:47:17 -0500

    Add files via upload

---
+++ SpringbootApp/src/main/java/com/brent/springboot/controller/mainController.java
@@ -0,0 +1,14 @@
+package com.brent.springboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class mainController {
+	
+	@RequestMapping("/home")
+	public String message() {
+		return "BAM!";
+	}
+
+}
